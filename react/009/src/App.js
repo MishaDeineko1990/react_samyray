6de042347ff808eba154b637d471dfdbb8ef93b8
MishaDeineko1990 <40040773+MishaDeineko1990@users.noreply.github.com>
2021-03-02 17:00:07 +0200

    fghfgh

---
+++ react/009/src/App.js
@@ -0,0 +1,20 @@
+import logo from './logo.svg';
+import Header from './components/header.js'
+import Footer from './components/footer.js'
+import './App.css';
+
+const App = () => {
+  return (
+    <div className = "App">
+      <Header/>
+      <h1>Symple js</h1> 
+      <p>hi broo</p>
+      <Footer/>
+    </div>
+  );
+}
+
+
+
+
+export default App;
---
+++ react/009/src/components/header.js
@@ -0,0 +1,10 @@
+const Header = () => {
+    return (
+        <div>
+          <a href="#">Home | </a>
+          <a href="#">Contacts</a>
+        </div>
+    );
+  }
+
+  export default Header
