0bead0dffa1d4eb3a4ac4cff7b47eec96deb6481
MishaDeineko1990 <40040773+MishaDeineko1990@users.noreply.github.com>
2021-03-03 15:06:46 +0200

    sociallnet

---
+++ socialnet/src/App.js
@@ -0,0 +1,12 @@
+import logo from './logo.svg';
+import './App.css';
+
+const App = () =>  {
+  return (
+    <div className="App">
+      <p>Hallo World</p>
+    </div>
+  );
+}
+
+export default App;
