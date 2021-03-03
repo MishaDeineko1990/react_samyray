a372bf84cdc1ba3606e3ddcde4ba07a82b5925be
MishaDeineko1990 <40040773+MishaDeineko1990@users.noreply.github.com>
2021-03-03 16:57:45 +0200

    11_lasson

--- socialnet/src/App.js
+++ socialnet/src/App.js
@@ -3,8 +3,19 @@ import './App.css';
 
 const App = () =>  {
   return (
-    <div className="App">
+    <div className="app_wrapper">
       <p>Hallo World</p>
+      <header className="header">
+        <img src="" alt=""/>
+      </header>
+      <nav  className="nav">
+        <div></div>
+        <div></div>
+        <div></div>
+      </nav>
+      <div className="content">
+      
+      </div>
     </div>
   );
 }
